d2a19ebb03865f56a02d233ecd2ab1b76750cc95
Cameron Martin <cameronmartin123@gmail.com>
2015-09-28 23:37:24 +0100

    Added tests to test the order of variable declaration initialisation and function declaration assignment

--- test/unit_cases/all-input.js
+++ test/unit_cases/all-input.js
@@ -5,3 +5,8 @@ function test() {
 }
 
 expect(test).to.eq('');
+
+function test2() {}
+expect(test2).to.be.a('function');
+
+var test2 = '';
--- test/unit_cases/all-output.js
+++ test/unit_cases/all-output.js
@@ -1,7 +1,15 @@
+window.test = undefined;
+window.test2 = undefined;
+
 window.test = function() {
 
 };
 
+window.test2 = function() {};
 window.test = '';
 
 expect(test).to.eq('');
+
+expect(test2).to.be.a('function');
+
+window.test2 = '';
